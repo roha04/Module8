28cbe940fab6d47339112e748090aabfb3e93737
RostykVoloschak <98826968+roha04@users.noreply.github.com>
2023-09-03 16:44:16 +0300

    Update Recongnizer.java
    Update Recongnizer

--- Recongnizer.java
+++ Recongnizer.java
@@ -1,11 +1,14 @@
 public class Recongnizer extends Shape{
+   Shape shape=new Shape();
+
     public void printNameOfShape(Shape shape){
+        this.shape=shape;
 
         System.out.println(toString()+shape.getClass().getName());
     }
 
     @Override
     public double getArea() {
-        return 0;
+        return 0.0;
     }
 }
